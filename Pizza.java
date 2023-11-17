24e41aa356b798163d3ae546c1ddcdbcb5910c02
kol myers <118282955+empomz@users.noreply.github.com>
2023-11-17 14:37:04 -0500

    Add files via upload

---
+++ Pizza.java
@@ -0,0 +1,24 @@
+public class Pizza {
+
+    private String name;
+    private double price;
+    private String size;
+    private String crust;
+    private String[] toppings;
+
+    public Pizza(){
+
+    }
+
+    public Pizza(String name, double price, String size, String crust, String[] toppings){
+
+        this.name = name;
+        this.price = price;
+        this.size = size;
+        this.crust = crust;
+        this.toppings = toppings;
+
+    }
+
+
+}
---
+++ Side.java
@@ -0,0 +1,15 @@
+public class Side {
+
+    private String name;
+    private double price;
+
+    public Side(){
+
+    }
+
+    public Side(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+
+}
