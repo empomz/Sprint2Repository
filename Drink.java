85c07930aa4198a6307953eb2636f3318ba36e4b
kol myers <118282955+empomz@users.noreply.github.com>
2023-11-17 14:36:23 -0500

    Add files via upload

---
+++ Drink.java
@@ -0,0 +1,25 @@
+public class Drink {
+
+    private String name;
+    private String size;
+    private double price;
+
+    public Drink(){
+
+    }
+
+    public Drink(String name, String size, double price){
+        this.name = name;
+        this.size = size;
+        this.price = price;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+}
---
+++ MenuItem.java
@@ -0,0 +1,31 @@
+public class MenuItem {
+
+    private String name;
+    private double price;
+
+    public MenuItem(){
+
+    }
+
+    public MenuItem(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+}
